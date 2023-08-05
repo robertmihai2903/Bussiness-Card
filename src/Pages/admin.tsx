aef0afb2dc2c8d6d157bd8163afab8d7d1f15e59
Robert Mocanu <robert.mocanu@teads.tv>
2023-08-05 12:11:14 +0300

    Admin page

--- src/Pages/admin.tsx
+++ src/Pages/admin.tsx
@@ -3,7 +3,8 @@ import {Button, Input} from "@mui/material";
 import {useNavigate} from "react-router";
 import {useContext, useEffect, useState} from "react";
 import {MainContext} from "../contexts";
-import {doc, setDoc, addDoc, collection, query, where, getDocs} from "firebase/firestore";
+import {doc, setDoc, addDoc, collection, query, where, getDocs} from "firebase/firestore"
+import './admin.css'
 
 export enum Preview {
     BUSINESS_CARD = 'business_card',
@@ -24,14 +25,25 @@ export function AdminPage() {
 
         for (let i = 0; i < orderedProducts; i++) {
             const hexCode = random_hex_code()
-            await addDoc(collection(db, 'products'), {
+            addDoc(collection(db, 'products'), {
                 activated: false,
                 unlockCode: hexCode,
                 name: "New Product",
                 preview: Preview.BUSINESS_CARD
+            }).then((data) => {
+                setProducts((prev) => [...prev, {
+                    id: data.id, activated: false,
+                    unlockCode: hexCode,
+                    name: "New Product",
+                    preview: Preview.BUSINESS_CARD
+                }])
             })
         }
 
+    }
+
+    const copyLink = (productId: string) => {
+        navigator.clipboard.writeText(`https://bussiness-card-bda7f.firebaseapp.com/show-product?product_id=${productId}`)
     }
 
     useEffect(() => {
@@ -52,11 +64,20 @@ console.log(products)
                 setOrderedProducts(e.target.value)
             }}/>
             <Button onClick={createProducts}>get products</Button>
+            <div className={"admin-products"}>
             {products.map((product) => (
                 <div>
                     <span>{product.id}</span>
+                    <br/>
+                    <span>{product.unlockCode}</span>
+                    <Button onClick={() => {
+                        copyLink(product.id)
+                    }}>Link</Button>
+                    <br/>
+                    <br/>
                 </div>
             ))}
+            </div>
         </div>
 
 
