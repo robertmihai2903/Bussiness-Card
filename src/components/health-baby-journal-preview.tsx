eed113d12f2502213d50ac2b80164ad2af8c8bcc
Robert Mocanu <robert.mocanu@teads.tv>
2024-08-31 21:04:16 +0300

    Feedback

--- src/components/health-baby-journal-preview.tsx
+++ src/components/health-baby-journal-preview.tsx
@@ -11,23 +11,6 @@ export function HealthBabyJournalPreview() {
     return <div className={"j-segment-container"}>
         <h2 className={"j-preview-title"}>Medical Records</h2>
         <MediaPreview value={medicalRecords}/>
-        <h2 className={"j-preview-title"}>Mom</h2>
-        <ProfilePicturePreview asset={mother.profilePicture[0]}/>
-        <SmallInfoPreview label={"name"} info={mother.name}/>
-        <SmallInfoPreview label={"allergies"} info={mother.allergies}/>
-        <SmallInfoPreview label={"diseases"} info={mother.diseases}/>
-        <SmallInfoPreview label={"chronic aversions"} info={mother.chronicAversions}/>
-        <SmallInfoPreview label={"Blood type"} info={mother.bloodType}/>
-
-        <h2 className={"j-preview-title"}>Dad</h2>
-        <ProfilePicturePreview asset={father.profilePicture[0]}/>
-        <SmallInfoPreview label={"name"} info={father.name}/>
-        <SmallInfoPreview label={"allergies"} info={father.allergies}/>
-        <SmallInfoPreview label={"diseases"} info={father.diseases}/>
-        <SmallInfoPreview label={"chronic aversions"} info={father.chronicAversions}/>
-        <SmallInfoPreview label={"Blood type"} info={father.bloodType}/>
-
-
 
         <h2 className={"j-preview-title"}>Allergies, Health Info, Vaccines</h2>
         <LargeInfoPreview label={"Health Problems"} info={healthProblems}/>
@@ -36,5 +19,21 @@ export function HealthBabyJournalPreview() {
         <SmallInfoPreview label={"medication"} info={medication}/>
         <SmallInfoPreview label={"chronic aversions"} info={chronicAversions}/>
         <LargeInfoPreview label={"Other"} info={otherHealthConditions}/>
+
+        <h2 className={"j-preview-title"}>Mom</h2>
+        <ProfilePicturePreview asset={mother.profilePicture[0]}/>
+        <SmallInfoPreview label={"name"} info={mother.name}/>
+        <SmallInfoPreview label={"allergies"} info={mother.allergies}/>
+        <SmallInfoPreview label={"diseases"} info={mother.diseases}/>
+        <SmallInfoPreview label={"Chronic Adverse Reaction"} info={mother.chronicAversions}/>
+        <SmallInfoPreview label={"Blood type"} info={mother.bloodType}/>
+
+        <h2 className={"j-preview-title"}>Dad</h2>
+        <ProfilePicturePreview asset={father.profilePicture[0]}/>
+        <SmallInfoPreview label={"name"} info={father.name}/>
+        <SmallInfoPreview label={"allergies"} info={father.allergies}/>
+        <SmallInfoPreview label={"diseases"} info={father.diseases}/>
+        <SmallInfoPreview label={"Chronic Adverse Reaction"} info={father.chronicAversions}/>
+        <SmallInfoPreview label={"Blood type"} info={father.bloodType}/>
     </div>
 }
